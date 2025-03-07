fix: Adding new neurons without natural gradient step. (#74)
* Fix compute_optimal_updates when zero_delta=True. The computation of the optimal added neurons of a growing module uses self.delta_raw which is not set to zero if zero_delta=True in the current implementation. This should be included in the tests but might be tricky to do so.

* Fix compute_optimal_updates when zero_delta=True. The computation of the optimal added neurons of a growing module uses self.delta_raw which is not set to zero if zero_delta=True in the current implementation. This should be included in the tests but might be tricky to do so.

* Fix compute_optimal_updates when zero_delta=True. The computation of the optimal added neurons of a growing module uses self.delta_raw which is not set to zero if zero_delta=True in the current implementation. This should be included in the tests but might be tricky to do so.

* fix: remove old test code

* enh: add a new example

* fix: correct example

* fix: adding new test

* fix: correct test

* fix: test to solve doc error

* fix: new test for thumbnail

* fix: debug in actions

* fix: debug doc building

* fix: remove debug

* fix: minor bugs/warn in doc generation

* fix: correct doc generation error

---------

Co-authored-by: Sylvain Chevallier <sylain.chevallier@universite-paris-saclay.fr>
Co-authored-by: Sylvain Chevallier <sylvain.chevallier@universite-paris-saclay.fr>

src/gromo/modules/growing_module.py
@@ -939,7 +939,7 @@ class GrowingModule(torch.nn.Module):
     @property
     def tensor_n(self) -> torch.Tensor:
         """
-        Compute the tensor N for the layer with the current M_-2, C and optimal delta.
+        Compute the tensor N for the layer with the current M_{-2}, C and optimal delta.
 
         Returns
         -------
@@ -1267,7 +1267,7 @@ class GrowingModule(torch.nn.Module):
         update_previous: bool
             whether to change the previous layer extended_output_layer
         zero_delta: bool
-            if True, set the optimal delta to zero
+            if True, compute the optimal added neurons without performing the natural gradient step.
         dtype: torch.dtype
             dtype for the computation of the optimal delta and added parameters
 
@@ -1282,6 +1282,7 @@ class GrowingModule(torch.nn.Module):
                 self.optimal_delta_layer.weight.data.zero_()
                 if self.optimal_delta_layer.bias is not None:
                     self.optimal_delta_layer.bias.data.zero_()
+            self.delta_raw.zero_()
 
         if self.previous_module is None:
             return  # FIXME: change the definition of the function

src/gromo/modules/linear_growing_module.py
@@ -622,7 +622,7 @@ class LinearGrowingModule(GrowingModule):
     @property
     def tensor_n(self) -> torch.Tensor:
         """
-        Compute the tensor N for the layer with the current M_-2, P and optimal delta.
+        Compute the tensor N for the layer with the current M_{-2}, P and optimal delta.
 
         Returns
         -------
@@ -705,8 +705,8 @@ class LinearGrowingModule(GrowingModule):
             extension of the weight matrix of the layer if None,
             the layer is extended with zeros
             should be of shape:
-             - (out_features, in_features + added_in_features) if added_in_features > 0
-             - (out_features + added_out_features, in_features) if added_out_features > 0
+            - (out_features, in_features + added_in_features) if added_in_features > 0
+            - (out_features + added_out_features, in_features) if added_out_features > 0
         bias_extension: torch.Tensor of shape (out_features + added_out_features,)
             extension of the bias vector of the layer if None,
             the layer is extended with zeros
@@ -1094,90 +1094,3 @@ class LinearGrowingModule(GrowingModule):
                 )
 
         return alpha_weight, alpha_bias, omega, self.eigenvalues_extension
-
-
-if __name__ == "__main__":
-    l1 = LinearGrowingModule(
-        1, 1, use_bias=True, post_layer_function=torch.nn.ReLU(), name="l1"
-    )
-    l2 = LinearGrowingModule(
-        1,
-        1,
-        use_bias=True,
-        previous_module=l1,
-        post_layer_function=torch.nn.ReLU(),
-        name="l2",
-    )
-    l3 = LinearGrowingModule(1, 1, use_bias=True, previous_module=l2, name="l3")
-    x = torch.randn(200, 1, device=global_device())
-    net = torch.nn.Sequential(l1, l2, l3)
-
-    print(net)
-
-    for layer in net:
-        layer.init_computation()
-
-    for layer in net:
-        print(layer.__str__(verbose=1))
-
-    y = net(x)
-    loss = torch.norm(y)
-    print(f"loss: {loss}")
-    loss.backward()
-
-    for layer in net:
-        layer.update_computation()
-
-        layer.compute_optimal_updates()
-
-    for layer in net:
-        layer.reset_computation()
-
-    l1.delete_update()
-    l3.delete_update()
-
-    l2.scaling_factor = 1
-
-    print(f"{l2.first_order_improvement=}")
-    print(f"{l2.weight=}")
-    print(f"{l2.bias=}")
-    print(f"{l2.optimal_delta_layer=}")
-    print(f"{l2.parameter_update_decrease=}")
-    print(f"{l2.extended_input_layer=}")
-    print(f"{l2.extended_input_layer.weight=}")
-    print(f"{l2.extended_input_layer.bias=}")
-    print(f"{l1.extended_output_layer=}")
-    print(f"{l2.eigenvalues_extension=}")
-
-    x_ext = None
-    for layer in net:
-        x, x_ext = layer.extended_forward(x, x_ext)
-
-    new_loss = torch.norm(x)
-    print(f"loss: {new_loss}, {loss - new_loss} improvement")
-    l2.apply_change()
-
-    print("------- New weights -------")
-    print(f"{l1.weight=}")
-    print(f"{l2.weight=}")
-    print(f"{l3.weight=}")
-    print("------- New biases -------")
-    print(f"{l1.bias=}")
-    print(f"{l2.bias=}")
-    print(f"{l3.bias=}")
-
-    for layer in net:
-        layer.init_computation()
-
-    for layer in net:
-        print(layer.__str__(verbose=2))
-
-    y = net(x)
-    loss = torch.norm(y)
-    print(f"loss: {loss}")
-    loss.backward()
-
-    for layer in net:
-        layer.update_computation()
-
-        layer.compute_optimal_updates()